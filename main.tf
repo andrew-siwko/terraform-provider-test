c698c77c3d3f8fc046716a7110601373d223dde7
Andrew Siwko <asiwko@siwko.org>
2026-02-25 15:14:59 -0500

    change the name of the provider and add a variable

--- main.tf
+++ main.tf
@@ -1,12 +1,16 @@
 # main.tf
 terraform {
   required_providers {
-    hashivar = {
+    mirror = {
       source = "registry.terraform.io/andrew/property-mirror"
     }
   }
 }
 
-resource "hashivar_variable" "rhel_test" {
+resource "mirror_variable" "rhel_test" {
   value = "Running on RHEL 9"
+}
+
+resource "mirror_variable" "rhel_test_2" {
+  value = "Also Running on RHEL 9"
 }
