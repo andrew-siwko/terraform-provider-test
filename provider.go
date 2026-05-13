dbc1ea4613d1c9a299dbf66dd1029d750b65da51
Andrew Siwko <asiwko@siwko.org>
2026-05-13 16:15:09 -0400

    trimmed imports ; added data source

--- provider.go
+++ provider.go
@@ -7,12 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	provschema "github.com/hashicorp/terraform-plugin-framework/provider/schema"
-	// "github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	// resschema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	// "github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-	// "github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
-	// "github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 func New() provider.Provider {
@@ -32,8 +27,11 @@ func (p *hashivarProvider) Schema(_ context.Context, _ provider.SchemaRequest, r
 // Fixed: Added pointer (*) to ConfigureResponse
 func (p *hashivarProvider) Configure(_ context.Context, _ provider.ConfigureRequest, _ *provider.ConfigureResponse) {}
 
-func (p *hashivarProvider) DataSources(_ context.Context) []func() datasource.DataSource { return nil }
-
+func (p *hashivarProvider) DataSources(_ context.Context) []func() datasource.DataSource {
+    return []func() datasource.DataSource{
+        NewCoffeeDataSource, // This refers to the function in our other file
+    }
+}
 func (p *hashivarProvider) Resources(_ context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
 		NewVariableResource,
