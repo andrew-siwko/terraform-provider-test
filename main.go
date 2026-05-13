c09f6e19fad82f7062d1720f19704f50e9862bf8
Andrew Siwko <asiwko@siwko.org>
2026-05-13 16:14:48 -0400

    trimmed imports ; removed comment

--- main.go
+++ main.go
@@ -4,19 +4,10 @@ import (
 	"context"
 	"log"
 
-	// "github.com/hashicorp/terraform-plugin-framework/datasource"
-	// "github.com/hashicorp/terraform-plugin-framework/provider"
-	// provschema "github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	// "github.com/hashicorp/terraform-plugin-framework/resource"
-	// resschema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	// "github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-	// "github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
-	// "github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 func main() {
-	// The Serve function and Opts live in the providerserver package in recent versions
 	err := providerserver.Serve(context.Background(), New, providerserver.ServeOpts{
 		Address: "registry.terraform.io/andrew/property-mirror",
 	})
