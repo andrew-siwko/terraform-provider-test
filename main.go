a64e8368465dffc1c7eaa357914c9dac422a73f8
Andrew Siwko <asiwko@siwko.org>
2026-05-14 13:11:18 -0400

    let's use stdout

--- main.go
+++ main.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
 func main() {
-	log.Print("Starting terraform-provider-test")
+	fmt.Print("Starting terraform-provider-test\n")
 	err := providerserver.Serve(context.Background(), New, providerserver.ServeOpts{
 		Address: "registry.terraform.io/andrew/property-mirror",
 	})
 
 	if err != nil {
+		fmt.Print("Ending terraform-provider-test\n")
 		log.Fatal(err.Error())
 	}
-	log.Print("Ending terraform-provider-test")
+	fmt.Print("Ending terraform-provider-test\n")
 }
 
