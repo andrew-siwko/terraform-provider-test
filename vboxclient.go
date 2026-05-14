551e10cd28931dbcaa83d5e022b46727639e2b30
Andrew Siwko <asiwko@siwko.org>
2026-05-14 14:59:32 -0400

    added a function to get machine information

--- vboxclient.go
+++ vboxclient.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"github.com/andrew/terraform-provider-test/vboxapi" // The generated code
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hooklift/gowsdl/soap"
 )
 
@@ -44,4 +45,65 @@ func (c *VBoxClient) GetVMNames() ([]string, error) {
 	})
 
 	return names, nil
+}
+
+func (c *VBoxClient) GetDetailedVMs() ([]vmModel, error) {
+	soapClient := soap.NewClient(c.Endpoint)
+	service := vboxapi.NewVboxPortType(soapClient)
+
+	resp, err := service.IWebsessionManager_logon(&vboxapi.IWebsessionManager_logon{
+		Username: c.Username, // Can be ""
+		Password: c.Password, // Can be ""
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	vboxHandle := resp.Returnval
+
+	// Get Machine Handles
+	machinesResp, err := service.IVirtualBox_getMachines(&vboxapi.IVirtualBox_getMachines{
+		This: vboxHandle,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var results []vmModel
+
+	for _, handle := range machinesResp.Returnval {
+		// Get Name
+		n, _ := service.IMachine_getName(&vboxapi.IMachine_getName{This: handle})
+		
+		// Get Memory (VirtualBox returns MB)
+		m, _ := service.IMachine_getMemorySize(&vboxapi.IMachine_getMemorySize{This: handle})
+		
+		// Get CPU Count
+		cp, _ := service.IMachine_getCPUCount(&vboxapi.IMachine_getCPUCount{This: handle})
+		
+		// Get State
+		s, _ := service.IMachine_getState(&vboxapi.IMachine_getState{This: handle})
+
+		var stateString string
+		if s != nil && s.Returnval != nil {
+			stateString = string(*s.Returnval)
+		} else {
+			stateString = "Unknown"
+		}
+
+
+		results = append(results, vmModel{
+			Name:   types.StringValue(n.Returnval),
+			Memory: types.Int64Value(int64(m.Returnval)),
+			CPUs:   types.Int64Value(int64(cp.Returnval)),
+			State:  types.StringValue(string(stateString)),
+		})
+	}
+
+	// Always Logoff!
+	service.IWebsessionManager_logoff(&vboxapi.IWebsessionManager_logoff{
+		RefIVirtualBox: vboxHandle,
+	})
+
+	return results, nil
 }
