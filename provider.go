1fd0c156818f068ae8b3ff0d290bdfd1beafdb1d
Andrew Siwko <asiwko@siwko.org>
2026-05-14 10:26:21 -0400

    add datasource to provider

--- provider.go
+++ provider.go
@@ -38,3 +38,50 @@ func (p *hashivarProvider) Resources(_ context.Context) []func() resource.Resour
 	}
 }
 
+var _ datasource.DataSource = &vmsDataSource{}
+
+type vmsDataSource struct {
+	client *VBoxClient
+}
+
+func (d *vmsDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_vms"
+}
+
+// Configure allows the provider to pass the initialized client to the data source
+func (d *vmsDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*VBoxClient)
+	if !ok {
+		resp.Diagnostics.AddError("Unexpected Data Source Configure Type", "Expected *VBoxClient")
+		return
+	}
+
+	d.client = client
+}
+
+func (d *vmsDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"names": schema.ListAttribute{
+				ElementType: types.StringType,
+				Computed:    true,
+			},
+		},
+	}
+}
+
+func (d *vmsDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var state struct {
+		Names []string `tfsdk:"names"`
+	}
+
+	// This is where you would call d.client.Endpoint and use a SOAP library
+	// to fetch the real names from your RHEL 9 VirtualBox instance.
+	state.Names = []string{"rhel9-vm-01", "dev-server", "test-node"}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+}
