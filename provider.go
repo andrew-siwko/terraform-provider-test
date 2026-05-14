71e62c20d39aaf4fcb0c88c49050c69d01af0a50
Andrew Siwko <asiwko@siwko.org>
2026-05-14 11:00:16 -0400

    move struct and schema to data source

--- provider.go
+++ provider.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	// "log"
+   	// "fmt"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
@@ -40,10 +40,6 @@ func (p *hashivarProvider) Resources(_ context.Context) []func() resource.Resour
 
 var _ datasource.DataSource = &vmsDataSource{}
 
-type vmsDataSource struct {
-	client *VBoxClient
-}
-
 func (d *vmsDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
 	resp.TypeName = req.ProviderTypeName + "_vms"
 }
@@ -63,25 +59,3 @@ func (d *vmsDataSource) Configure(_ context.Context, req datasource.ConfigureReq
 	d.client = client
 }
 
-func (d *vmsDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
-	resp.Schema = schema.Schema{
-		Attributes: map[string]schema.Attribute{
-			"names": schema.ListAttribute{
-				ElementType: types.StringType,
-				Computed:    true,
-			},
-		},
-	}
-}
-
-func (d *vmsDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
-	var state struct {
-		Names []string `tfsdk:"names"`
-	}
-
-	// This is where you would call d.client.Endpoint and use a SOAP library
-	// to fetch the real names from your RHEL 9 VirtualBox instance.
-	state.Names = []string{"rhel9-vm-01", "dev-server", "test-node"}
-
-	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
-}
